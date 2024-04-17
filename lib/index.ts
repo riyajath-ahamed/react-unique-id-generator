12fe1dafad5f5f2314e62f96b7b2eaa61b434b16
riyajath-ahamed <riyajatha@gmail.com>
2024-04-17 23:46:50 +0530

    init

---
+++ lib/index.ts
@@ -0,0 +1,6 @@
+import nextId, { resetId, setPrefix } from "./nextId";
+import useId from "./useid";
+import generateUniqueId from "./custFormat";
+
+
+export { nextId as default, resetId, setPrefix, useId , generateUniqueId };
---
+++ lib/useid.ts
@@ -0,0 +1,30 @@
+import React from "react";
+import nextId from "./nextId";
+
+const getIds = (count: number, prefix?: string | undefined) => {
+  const ids: string[] = [];
+  for (let i = 0; i < count; i++) {
+    ids.push(nextId(prefix));
+  }
+  return ids;
+};
+
+function usePrevious(value: unknown) {
+  const ref = React.useRef<unknown>();
+  React.useEffect(() => {
+    ref.current = value;
+  });
+  return ref.current;
+}
+
+export default function useId(count = 1, prefix?: string): string[] {
+  const idsListRef = React.useRef<string[]>([]);
+  const prevCount = usePrevious(count);
+  const prevPrefix = usePrevious(prefix);
+
+  if (count !== prevCount || prevPrefix !== prefix) {
+    idsListRef.current = getIds(count, prefix);
+  }
+
+  return idsListRef.current;
+}
